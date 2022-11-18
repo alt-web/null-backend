accb3ba64cdeb590137be6854a71b7afd550f33c
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-18 19:45:47 +0500

    Create messaging app

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,4 +1,4 @@
-"""nullchan_backend URL Configuration
+"""Messaging URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from messaging import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('messaging.urls')),
+    path('', views.home, name='home'),
 ]
