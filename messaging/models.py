e341a33c005ef3cbcfcc12e08eca53dc71083718
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-18 20:04:12 +0500

    Add boards

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    code = models.CharField(max_length=6)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'/{self.code}/ - {self.name}'
