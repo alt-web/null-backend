7ba1d3edada2d4cc5a77f6b8c37533aeb15245ff
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-19 14:42:33 +0500

    Add threads and replies

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from messaging.models import Board
+from messaging.models import Board, Thread, Reply
 
 # Register your models here.
 admin.site.register(Board)
+admin.site.register(Thread)
+admin.site.register(Reply)
--- messaging/models.py
+++ messaging/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Board(models.Model):
     code = models.CharField(max_length=6)
     name = models.CharField(max_length=30)
@@ -8,3 +8,24 @@ class Board(models.Model):
 
     def __str__(self):
         return f'/{self.code}/ - {self.name}'
+
+
+class Post(models.Model):
+    """
+    Base class for threads and replies
+    """
+    body = models.CharField(max_length=1024)
+
+
+class Thread(Post):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body[:20]
+
+
+class Reply(Post):
+    origin = models.ForeignKey(Thread, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body[:20]
