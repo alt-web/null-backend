526c837272bf3a32d26c310829d1c294004d6422
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-01-03 12:10:37 +0500

    Improve preview generation process
    Add resizing step, improve type hints

--- messaging/utils.py
+++ messaging/utils.py
@@ -4,6 +4,7 @@ import io
 import urllib.parse
 import requests
 import json
+from typing import Union, BinaryIO
 from PIL import Image
 from mutagen.id3 import ID3
 from messaging.models import Attachment
@@ -27,21 +28,35 @@ def is_audio_file(mime_type: str) -> bool:
     # MP3 files sometimes recognised as octet-stream
     if mime_type == 'application/octet-stream':
         return True
-    
+
     return False
 
 
-def encode_webp(image_data):
+def compress_image(image_data: BinaryIO) -> io.BytesIO:
     """
-    Converts image to webp format
+    Resizes an image and saves it in a webp format
     """
-    img = Image.open(image_data).convert("RGB")
     buffer = io.BytesIO()
-    img.save(buffer, format="WebP")
+
+    # Load image
+    with Image.open(image_data) as img:
+        rgb_img = img.convert("RGB")
+
+        # Resize image
+        width, height = rgb_img.size
+        max_size = 200
+        ratio = max_size / width if width > height else max_size / height
+        width = int(width * ratio)
+        height = int(height * ratio)
+        final_img = rgb_img.resize((width, height))
+
+        # Save image
+        final_img.save(buffer, format="WebP")
+
     return buffer
 
 
-def get_audio_preview(file) -> Attachment:
+def get_audio_preview(file: BinaryIO) -> Union[Attachment, None]:
     """
     Tries to extract the cover from audio file.
     Returns None if any exception was raised.
@@ -52,7 +67,7 @@ def get_audio_preview(file) -> Attachment:
         image_buffer = io.BytesIO(image_data)
 
         # Convert to webp
-        image_data = encode_webp(image_buffer)
+        image_data = compress_image(image_buffer)
         image_data.seek(0)
 
         # Upload to ipfs
